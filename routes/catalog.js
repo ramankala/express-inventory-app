c595cf3f55872f1aed1dd4a2adf8e53490cc4f8d
Raman <rs.kala@hotmail.com>
2022-12-14 16:53:05 -0500

    Complete category routes for catelog

--- routes/catalog.js
+++ routes/catalog.js
@@ -35,3 +35,29 @@ router.get("/item/:id", item_controller.item_detail);
 router.get("/items", item_controller.item_list);
 
 /// CATEGORY ROUTES ///
+
+// GET request for creating Category.
+router.get("/category/create", category_controller.category_create_get);
+
+// POST request for creating Category.
+router.get("/category/create", category_controller.category_create_post);
+
+// GET request to delete Category.
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
+// POST request to delete Category.
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+// GET request to update Category.
+router.get("/category/:id/update", category_controller.category_update_get);
+
+// POST request to update Category.
+router.post("/category/:id/update", category_controller.category_update_post);
+
+// GET request for one Category.
+router.get("/category/:id", category_controller.category_detail);
+
+// GET request for list of all Categories.
+router.get("/categories", category_controller.category_list);
+
+module.exports = router;
