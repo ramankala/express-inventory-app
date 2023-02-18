d4ff105eb858480c632659b63bcb32b576540a99
Raman <rs.kala@hotmail.com>
2023-02-17 23:37:29 -0500

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog");  // Import routes for "catalog" area of site
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://expressinventory:expressinventory-db@inventory-cluster.emyamaj.mongodb.net/?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://expressinventory:expressinventory-db@inventory-cluster.emyamaj.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -26,6 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(compression());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
